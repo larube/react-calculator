632cd9835c4cde06a311796d669ba5f5e6eaac9d
Amador Rubal <amador.rubal@gmail.com>
2018-08-14 22:41:13 +0200

    chore(tool) Use module resolver

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { hot } from 'react-hot-loader';
-import './styles/fonts/fonts.scss';
-import './styles/main.scss';
+import 'styles/fonts/fonts.scss';
+import 'styles/main.scss';
 import Calculator from './containers/Calculator';
 
 const App = () => <Calculator />;
--- src/components/Output.js
+++ src/components/Output.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import OutputButton from './OutputButton';
+import OutputButton from 'components/OutputButton';
 
 const Output = ({ currentOperation, currentResult }) => (
   <div className="screen row">
