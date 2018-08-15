cf3b53171d77d67ddf2f655a1cfe6702a9bf15f6
Amador Rubal <amador.rubal@gmail.com>
2018-08-15 13:23:25 +0200

    feat(containers) Split app with small redux containers

---
+++ src/containers/OperatorButtons.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import ButtonOperator from 'components/ButtonOperator';
+
+import {
+  typingButton,
+  typingOperator,
+  typingFloatNumber,
+} from 'containers/actions';
+
+const OperatorButtons = ({ onClick }) => (
+  <div className="row">
+    {['/', '*', '+', '-'].map(value => (
+      <ButtonOperator value={value} onClick={onClick} key={value} />
+    ))}
+  </div>
+);
+
+const mapStateToProps = ({ currentOperation, istypingOperator }) => ({
+  currentOperation,
+  istypingOperator,
+});
+
+const mapDispatchToProps = (dispatch, ownProps) => ({
+  onClick: ({ target: { value } }) => {
+    const { currentOperation, istypingOperator } = ownProps;
+    if (istypingOperator && value !== '-') return;
+    dispatch(typingButton(`${currentOperation} ${value} `));
+    // seems to be legit to let user types -(-89)
+    dispatch(typingOperator(value !== '-'));
+    dispatch(typingFloatNumber(false));
+  },
+});
+
+export default compose(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(OperatorButtons);
